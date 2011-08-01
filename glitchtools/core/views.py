f973d053f9954937ec2416c58760d933f69b1d33
Noah Kantrowitz <noah@coderanger.net>
2011-07-31 17:00:12 -0700

    Redirect.

--- glitchtools/core/views.py
+++ glitchtools/core/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponseRedirect
 from django.template.response import TemplateResponse
 
 def comingsoon(request):
+    return HttpResponseRedirect(request.build_absolute_uri('/map/'))
     return TemplateResponse(request, 'glitchtools/core/comingsoon.html', {})
